456ea16ef3c99655d96fcb43ba096504ce5ab711
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2024-05-22 22:50:52 +0300

    feat: Added load function

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+import type
+
 
 TG_BOT_API_URL = "https://core.telegram.org/bots/api"
 PROJECT_PATH = os.getenv("PROJECT_PATH")
+html_path = f"{PROJECT_PATH}/html"
+html_name = "tg_bot_api.html"
 
 
 def save_html() -> None:
@@ -15,20 +19,49 @@ def save_html() -> None:
     :return: None
     """
 
-    global TG_BOT_API_URL, PROJECT_PATH
+    global TG_BOT_API_URL, html_path, html_name
 
-    answer = requests.get(TG_BOT_API_URL)
-    content = answer.content
+    response = requests.get(TG_BOT_API_URL)
+
+    if response.status_code != 200:
+        raise Exception("Request code not 200")
+
+    content = response.content
     soup = BeautifulSoup(content, "html.parser")
-    path = f"{PROJECT_PATH}/html"
-    name = "tg_bot_api.html"
 
-    if not os.path.exists(path):
-        os.makedirs(path)
+    if not os.path.exists(html_path):
+        os.makedirs(html_path)
 
-    with open(f"{path}/{name}", "w", encoding="utf-8") as file:
+    with open(f"{html_path}/{html_name}", "w", encoding="utf-8") as file:
         html = soup.prettify()
         file.write(html)
+
+
+def load_html() -> BeautifulSoup:
+    """
+    Loads the downloaded telegram bot api page and creates a BeautifulSoup object from it
+
+    :return: The BeautifulSoup object of the telegram bot api html page
+    """
+
+    global html_path, html_name
+
+    with open(f"{html_path}/{html_name}", "r", encoding="utf-8") as file:
+        html = file.read()
+        soup = BeautifulSoup(html)
+
+    return soup
+
+
+def parse_types() -> dict[str, list[type.Type]]:
+    """
+
+    :return:
+    """
+
+    soup = load_html()
+
+    return {}
 
 
 def main() -> int:
---
+++ src/type.py
@@ -0,0 +1,11 @@
+class Type:
+    def __init__(self, name: str, tg_type: str, optional: bool):
+        """
+        Initializes the Type class
+
+        :param name: Type name
+        :param tg_type: Telegram bot api type of type
+        :param optional: Is the type optional
+        """
+
+        pass
